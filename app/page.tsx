bb01cbdb96c45c83fc4ce657690b2d87c52b798e
talalcodes55 <talalalghazal55@gmail.com>
2025-08-17 13:03:52 +0300

    moved the table to a separate component and edited the font size

--- app/page.tsx
+++ app/page.tsx
@@ -1,92 +1,24 @@
+import DocsTable from "@/components/docs-table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-rubik grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="flex w-full justify-between text-end">
-        <h1 className="text-2xl">توثيق الانتهاكات في قرية رساس</h1>
+        <h1 className="text-[18px] font-semibold">توثيق الانتهاكات في قرية رساس</h1>
         <div className="text-end">
-          <h3 className="text-[20px]">اللجنة القانونية العليا لتقصي الحقائق في السويداء</h3>
+          <h3 className="text-[18px] font-semibold">اللجنة القانونية العليا لتقصي الحقائق في السويداء</h3>
           <br />
-          <h4 className="text-[20px]">لجنة قرية رساس</h4>
+          <h4 className="text-[18px] font-semibold">لجنة قرية رساس</h4>
         </div>
       </div>
-      <main className="flex flex-col gap-[32px] row-start-2 font-rubik items-center sm:items-end">
-        <span className="text-[19px]">الرجاء تحديد القسم المراد عرض التوثيقات منه</span>
-
+      <main className="flex flex-col gap-[32px] w-full row-start-2 font-rubik items-center">
+        <DocsTable/>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border font-mono border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
